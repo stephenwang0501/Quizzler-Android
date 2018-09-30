3a25465a08d95567befc7cc62f371c2ec2d6fa58
stephenwang0501 <zixuanwang0501@gmail.com>
2018-09-30 13:14:33 -0700

    Add save app status

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -1,6 +1,8 @@
 package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,18 +11,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-    // TODO: Declare constants here
-
+    //TODO: add constant variables here
+    private static final String INDEX = "CURRENT_INDEX";
+    private static final String SCORE = "CURRENT_SCORE";
+    private static final String DIALOG_SHOWN = "DIALOG_SHOWN";
 
     // TODO: Declare member variables here:
     Button mButtonTrue;
     Button mButtonFalse;
-    ProgressBar progressBar;
+    ProgressBar mProgressBar;
     TextView mScoreTextView;
     TextView mQuestionTextView;
+    AlertDialog mDialog;
     int mIndex;
-    int totalScore;
+    int mTotalScore;
+    boolean mDialogShown;
 
     // TODO: Uncomment to create question bank
     private TrueFalse[] mQuestionBank = new com.londonappbrewery.quizzler.TrueFalse[] {
@@ -65,18 +70,46 @@ public class MainActivity extends Activity {
             }
         });
 
-        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        progressBar.setMax(mQuestionBank.length);
-
-        mIndex = 0;
-        totalScore = 0;
-
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mProgressBar.setMax(mQuestionBank.length);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.setText(mQuestionBank[mIndex].getmQuestionID());
-
         mScoreTextView = (TextView) findViewById(R.id.score);
-        final String init_str = getString(R.string.initial_score);
-        mScoreTextView.setText(init_str);
+
+        if (savedInstanceState == null) {
+            reset(0, 0);
+            mDialogShown = false;
+        } else {
+            reset(savedInstanceState.getInt(INDEX), savedInstanceState.getInt(SCORE));
+            mDialogShown = savedInstanceState.getBoolean(DIALOG_SHOWN);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putInt(INDEX, mIndex);
+        bundle.putInt(SCORE, mTotalScore);
+        bundle.putBoolean(DIALOG_SHOWN, mDialogShown);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mDialog != null && mDialog.isShowing()) {
+            mDialogShown = true;
+            mDialog.cancel();
+        } else {
+            mDialogShown = false;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mDialogShown) {
+            setDialog();
+        }
     }
 
     private void updateTotalScore(final boolean userInput) {
@@ -85,7 +118,7 @@ public class MainActivity extends Activity {
         }
 
         if (mQuestionBank[mIndex].getmAnswer() == userInput) {
-            ++totalScore;
+            ++mTotalScore;
         }
     }
 
@@ -93,17 +126,58 @@ public class MainActivity extends Activity {
         ++mIndex;
         if (mIndex >= mQuestionBank.length) {
             mIndex = mQuestionBank.length;
-            final String toastText = getString(R.string.final_score) + " " + totalScore;
-            Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG).show();
+            setDialog();
             return;
         }
         mQuestionTextView.setText(mQuestionBank[mIndex].getmQuestionID());
-        progressBar.setProgress(mIndex+1);
+        mProgressBar.setProgress(mIndex+1);
     }
 
     private void setScoreTextView() {
         final String scoreText = getString(R.string.current_score) +
-                " " + totalScore + "/" + mQuestionBank.length;
+                " " + mTotalScore + "/" + mQuestionBank.length;
         mScoreTextView.setText(scoreText);
+    }
+
+    private void reset(final int index, final int score) {
+        mIndex = index;
+        mTotalScore = score;
+        mProgressBar.setProgress(mIndex);
+        final String scoreText = getString(R.string.initial_score) + " " + mTotalScore + "/" + mQuestionBank.length;
+        mScoreTextView.setText(scoreText);
+        if (mIndex < mQuestionBank.length) {
+
+        } else {
+            mQuestionTextView.setText(mQuestionBank[mQuestionBank.length-1].getmQuestionID());
+        }
+    }
+
+    private void setDialog() {
+        final String dialogText = getString(R.string.final_score) + " " + mTotalScore;
+        mDialog = new AlertDialog.Builder(this).create();
+        mDialog.setTitle(R.string.result_dialog_title);
+        mDialog.setCancelable(false);
+        mDialog.setMessage(dialogText);
+        mDialog.setButton(
+                AlertDialog.BUTTON_POSITIVE,
+                getString(R.string.result_dialog_retake),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        reset(0, 0);
+                    }
+                });
+        mDialog.setButton(
+                AlertDialog.BUTTON_NEGATIVE,
+                getString(R.string.result_dialog_exit),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }
+        );
+        mDialog.show();
+        mDialogShown = true;
     }
 }
